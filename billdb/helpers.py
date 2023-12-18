e282c22496a9aa03d6539bf0d4281703ec2eab4c
lumpsoid <mmgeeki@gmail.com>
2023-12-18 22:06:28 +0100

    reorginized

--- billdb/helpers.py
+++ billdb/helpers.py
@@ -4,8 +4,11 @@ def is_valid_time(time_string):
     '''
     Check if string is in right format YYYY-MM-DD
     '''
+    if time_string is None:
+        raise ValueError('time_string is None')
     try:
         strptime(time_string, '%Y-%m-%d')
         return True
     except ValueError:
+        raise
         return False
