4889dfde73f96e23ec6239731f124f72b48e0b7b
lumpsoid <mmgeeki@gmail.com>
2024-01-17 20:54:45 +0100

    splite get_bill_info into several functions

--- billdb/_bill.py
+++ billdb/_bill.py
@@ -13,6 +13,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 
+
 class Bill:
     connector = None
     cursor = None
@@ -23,7 +24,7 @@ class Bill:
         if cls.connector is None:
             cls.connector = sqlite3.connect(path_to_db)
             cls.cursor = cls.connector.cursor()
-    
+
     @classmethod
     def close_sqlite(cls) -> None:
         if cls.connector is not None:
@@ -48,14 +49,16 @@ class Bill:
         cls.cursor.execute(unique_name_query)
         data_unique_names = cls.cursor.fetchall()
         if len(data_unique_names) == 0:
-            Bill.LOGGER.info('No new unique names of items')
+            Bill.LOGGER.info("No new unique names of items")
         else:
             cls.cursor.execute(add_unique_names_query)
             result = cls.commit_transaction()
             if result == 1:
-                Bill.LOGGER.info(f'{len(data_unique_names)} new unique items')
+                Bill.LOGGER.info(f"{len(data_unique_names)} new unique items")
             else:
-                Bill.LOGGER.warning('Something wrong with transaction. Is there a connector?')
+                Bill.LOGGER.warning(
+                    "Something wrong with transaction. Is there a connector?"
+                )
 
     @classmethod
     def commit_transaction(cls) -> int:
@@ -65,30 +68,28 @@ class Bill:
             output = 1
         return output
 
-
     def __init__(
-            self,
-            path_to_db: Union[str, None] = None,
-            name: Union[str, None] = None,
-            date: Union[str, None] = None,
-            price: Union[int, None] = None,
-            currency: Union[str, None] = None,
-            exchange_rate: Union[str, None] = None,
-            country: Union[str, None] = None,
-            items: Union[List[Item], None] = None,
-            tags: Union[str, None] = None
+        self,
+        path_to_db: Union[str, None] = None,
+        name: Union[str, None] = None,
+        date: Union[str, None] = None,
+        price: Union[int, None] = None,
+        currency: Union[str, None] = None,
+        exchange_rate: Union[str, None] = None,
+        country: Union[str, None] = None,
+        items: Union[List[Item], None] = None,
+        tags: Union[str, None] = None,
     ):
-
         if path_to_db and Bill.connector is None:
             if not isinstance(path_to_db, str):
-                raise ValueError('path_to_db must be str')
+                raise ValueError("path_to_db must be str")
             Bill.connect_to_sqlite(path_to_db)
 
         if items is None:
             items = []
-        
+
         if date and not is_valid_time(date):
-            raise ValueError('date must be in ISO format YYYY-MM-DD')
+            raise ValueError("date must be in ISO format YYYY-MM-DD")
 
         self.db = path_to_db
         self.timestamp = time.time_ns()
@@ -104,10 +105,9 @@ class Bill:
         self.bill_text = None
         self.dup_list = None
 
-
     def __repr__(self):
         # params = vars(self)
-        params = f'timestamp: {self.timestamp}\nname: {self.name}\ndate: {self.date}\nprice: {self.price}\ncurrency: {self.currency}\ncountry: {self.country}\nitems: {len(self.items)}\ntags: {self.tags}\nlink: {self.link}'
+        params = f"timestamp: {self.timestamp}\nname: {self.name}\ndate: {self.date}\nprice: {self.price}\ncurrency: {self.currency}\ncountry: {self.country}\nitems: {len(self.items)}\ntags: {self.tags}\nlink: {self.link}"
         return params
 
     def add_item(self, name, price, price_one, quantity, photo_path=None) -> object:
@@ -116,13 +116,24 @@ class Bill:
             price=price,
             price_one=price_one,
             quantity=quantity,
-            photo_path=photo_path
+            photo_path=photo_path,
         )
         self.items.append(item)
         return self
 
-    def update_info(self, name=None, date=None, price=None, currency=None, exchange_rate=None, country=None, bill_text=None, items=None, tags=None):
-        '''Updating info about the bill if provided.'''
+    def update_info(
+        self,
+        name=None,
+        date=None,
+        price=None,
+        currency=None,
+        exchange_rate=None,
+        country=None,
+        bill_text=None,
+        items=None,
+        tags=None,
+    ):
+        """Updating info about the bill if provided."""
         if name:
             self.name = name
         if date:
@@ -143,25 +154,34 @@ class Bill:
             self.exchange_rate = exchange_rate
         return
 
-
     def check_duplicates(self) -> None:
-        Bill.cursor.execute(f'''
+        Bill.cursor.execute(
+            f"""
         SELECT id, name, dates, price, currency, bill
         FROM bills
         WHERE
             dates = '{self.date}'
             AND price = {self.price}
             AND currency = '{self.currency}';
-        ''')
+        """
+        )
 
         self.dup_list = Bill.cursor.fetchall()
-    
+
     def from_qr(self, link) -> object:
-        if re.match(r'https://suf.purs.gov.rs', link):
-            Bill.LOGGER.debug('using serbian parser.')
+        if re.match(r"https://suf.purs.gov.rs", link):
+            Bill.LOGGER.debug("using serbian parser.")
             # name, date, price, currency, country, bill_text, items
-            self.name, self.date, self.price, self.currency, self.country, self.bill_text, self.items = _serbia.get_bill_info(link)
-            self.exchange_rate = '1'
+            (
+                self.name,
+                self.date,
+                self.price,
+                self.currency,
+                self.country,
+                self.bill_text,
+                self.items,
+            ) = _serbia.get_bill_info(link)
+            self.exchange_rate = "1"
             self.link = link
 
         return self
@@ -172,49 +192,70 @@ class Bill:
         if self.db and Bill.connector is None:
             Bill.connect_to_sqlite(self.db)
         if self.db is None and Bill.connector is None:
-            raise ValueError('add connection to database')
+            raise ValueError("add connection to database")
         if not is_valid_time(self.date):
-            raise ValueError('date must be in ISO format YYYY-MM-DD')
+            raise ValueError("date must be in ISO format YYYY-MM-DD")
         if self.name is None:
-            raise ValueError('name must be specified')
+            raise ValueError("name must be specified")
         if self.price is None:
-            raise ValueError('price must be specified')
+            raise ValueError("price must be specified")
         if self.currency is None:
-            raise ValueError('currency must be specified')
+            raise ValueError("currency must be specified")
         if self.exchange_rate is None:
-            if self.currency == 'rsd':
+            if self.currency == "rsd":
                 self.exchange_rate = 1
             else:
-                raise ValueError('exchange rate must be specified')
+                raise ValueError("exchange rate must be specified")
         if self.country is None:
-            raise ValueError('country must be specified')
+            raise ValueError("country must be specified")
         if self.tags is None:
             if self.link is None:
-                raise ValueError('tags must be specified')
+                raise ValueError("tags must be specified")
             self.tags = ""
 
         # checking duplicates
         self.check_duplicates()
         if len(self.dup_list) and not force_dup:
-            Bill.LOGGER.info(f'Maybe duplicates ({len(self.dup_list)}). Nothing was changed.')
+            Bill.LOGGER.info(
+                f"Maybe duplicates ({len(self.dup_list)}). Nothing was changed."
+            )
             return
 
         Bill.cursor.execute(
-                'INSERT INTO bills (id, name, dates, price, currency, exchange_rate, country, tag, link, bill) VALUES (?,?,?,?,?,?,?,?,?,?)',
-                (self.timestamp, self.name, self.date, self.price, self.currency, self.exchange_rate, self.country, self.tags, self.link, self.bill_text,)
+            "INSERT INTO bills (id, name, dates, price, currency, exchange_rate, country, tag, link, bill) VALUES (?,?,?,?,?,?,?,?,?,?)",
+            (
+                self.timestamp,
+                self.name,
+                self.date,
+                self.price,
+                self.currency,
+                self.exchange_rate,
+                self.country,
+                self.tags,
+                self.link,
+                self.bill_text,
+            ),
         )
         if self.items != []:
-            insert_query = 'INSERT INTO items (id, name, price, price_one, quantity) VALUES (?,?,?,?,?)'
+            insert_query = "INSERT INTO items (id, name, price, price_one, quantity) VALUES (?,?,?,?,?)"
             for item in self.items:
                 Bill.cursor.execute(
-                        insert_query,
-                        (self.timestamp, item.name, item.price, item.price_one, item.quantity,)
+                    insert_query,
+                    (
+                        self.timestamp,
+                        item.name,
+                        item.price,
+                        item.price_one,
+                        item.quantity,
+                    ),
                 )
         else:
-            Bill.LOGGER.info('items are empty.')
+            Bill.LOGGER.info("items are empty.")
 
         result = Bill.commit_transaction()
         if result == 1:
-            Bill.LOGGER.info('Transaction commited')
+            Bill.LOGGER.info("Transaction commited")
         else:
-            Bill.LOGGER.warning('Something wrong with transaction. Is there a connector?')
+            Bill.LOGGER.warning(
+                "Something wrong with transaction. Is there a connector?"
+            )
--- billdb/_parsers/_serbia.py
+++ billdb/_parsers/_serbia.py
@@ -8,85 +8,106 @@ from .._item import Item
 from .._utils.logging import get_logger
 
 
-def get_bill_info(link):
-    '''
+def get_bill_info(link: str) -> tuple:
+    """
     Parsing site with bill info.
 
     link: str
 
     Return: name, date, price, currency, country, bill_text, items
-    '''
-    LOGGER = get_logger(__name__)
-
-    response = requests.get(link, timeout=60)
-    LOGGER.info('status code: {}'.format(response.status_code))
-
+    """
     # metaparameters
-    re_site_junk = re.compile(r'\r\n\s+')        
-    token_xpath = '/html/head/script[5]'
+    token_xpath = "/html/head/script[5]"
     invoce_xpath = '//*[@id="invoiceNumberLabel"]'
     price_xpath = '//*[@id="totalAmountLabel"]'
     buy_date_xpath = '//*[@id="sdcDateTimeLabel"]'
     bill_xpath = '//*[@id="collapse3"]/div/pre'
     name_xpath = '//*[@id="shopFullNameLabel"]'
     token_search = r"viewModel\.Token\('(.*)'\);"
-    date_format = "%d.%m.%Y." # format of the date string
+
+    LOGGER = get_logger(__name__)
+
+    response = requests.get(link, timeout=60)
+    LOGGER.info("status code: {}".format(response.status_code))
 
     # Parse the HTML content
     dom = etree.HTML(response.content)
 
-    price = dom.xpath(price_xpath)[0].text
+    name = get_bill_name(dom, name_xpath)
+    bill_text = get_bill_text(dom, bill_xpath)
+    date = get_bill_buy_date(dom, buy_date_xpath, "%d.%m.%Y.", "%Y-%m-%d")
+    items = get_bill_items(dom, token_xpath, token_search, invoce_xpath)
+    price = get_bill_price(dom, price_xpath)
+    
+    currency = "rsd"
+    country = "serbia"
 
-    buy_date = dom.xpath(buy_date_xpath)[0].text
-    buy_date = re_site_junk.sub('', buy_date)
-    buy_date = buy_date.split(' ')[0]
-    buy_date = strptime(buy_date, date_format)
+    return (name, date, price, currency, country, bill_text, items)
 
-    bill = dom.xpath(bill_xpath)
 
-    # items fetching
+def get_bill_items(dom: str, token_xpath: str, token_search: str, invoce_xpath: str):
+    """_summary_
+
+    Args:
+        dom (str): parsed html dom
+        token_xpath (str): xpath to the token node
+        token_search (str): pattern to get token from node
+        invoce_xpath (str): xpath to the invoce node
+
+    Returns:
+        List(Item): Item's list
+    """
+    LOGGER = get_logger(__name__)
     token = re.search(token_search, dom.xpath(token_xpath)[0].text).group(1)
-    invoce_num = dom.xpath(invoce_xpath)[0].text.strip(' \r\n')
-    data_post = {
-        "invoiceNumber": invoce_num,
-        "token": token
-    }
-    post_r = requests.post('https://suf.purs.gov.rs//specifications', data=data_post)
+    invoce_num = dom.xpath(invoce_xpath)[0].text.strip(" \r\n")
+    data_post = {"invoiceNumber": invoce_num, "token": token}
+    post_r = requests.post("https://suf.purs.gov.rs//specifications", data=data_post)
     sleep(0.2)
-    post_r = requests.post('https://suf.purs.gov.rs//specifications', data=data_post)
-    json_data = json.loads(post_r.content.decode('utf-8'))
-    if json_data.get('Success') == False:
-        LOGGER.info("Items was not fetched. {}".format(link))
-        LOGGER.info('Retring...')
+    post_r = requests.post("https://suf.purs.gov.rs//specifications", data=data_post)
+    json_data = json.loads(post_r.content.decode("utf-8"))
+    if json_data.get("Success") == False:
+        LOGGER.info("Items was not fetched.")
+        LOGGER.info("Retring...")
         sleep(0.1)
-        post_r = requests.post('https://suf.purs.gov.rs//specifications', data=data_post)
-        json_data = json.loads(post_r.content.decode('utf-8'))
+        post_r = requests.post(
+            "https://suf.purs.gov.rs//specifications", data=data_post
+        )
+        json_data = json.loads(post_r.content.decode("utf-8"))
 
-    items_json = json_data.get('Items')
+    items_json = json_data.get("Items")
     items = []
     for item in items_json:
-        items.append(Item(
-            name=item.get('Name'),
-            price=item.get('Total'),
-            price_one=item.get('UnitPrice'),
-            quantity=item.get('Quantity'),
-            photo_path=None))
-    price = float(price.replace('.','').replace(',','.'))
-    name = dom.xpath(name_xpath)[0].text
-    currency = 'rsd'
-    country = 'serbia'
-    date = strftime("%Y-%m-%d", buy_date)
-    if len(bill) == 0:
-        bill_text = "check"
-    else:
-        bill_text = bill[0].text
+        items.append(
+            Item(
+                name=item.get("Name"),
+                price=item.get("Total"),
+                price_one=item.get("UnitPrice"),
+                quantity=item.get("Quantity"),
+                photo_path=None,
+            )
+        )
+    return items
 
-    return (
-        name,
-        date,
-        price,
-        currency,
-        country,
-        bill_text,
-        items
-    )
+def get_bill_buy_date(dom: str, buy_date_xpath: str, date_format_parse: str, date_format_output: str) -> str:
+    re_site_junk = re.compile(r"\r\n\s+")
+    buy_date = dom.xpath(buy_date_xpath)[0].text
+    buy_date = re_site_junk.sub("", buy_date)
+    buy_date = buy_date.split(" ")[0]
+    buy_date = strptime(buy_date, date_format_parse)
+    date = strftime(date_format_output, buy_date)
+    return date
+
+def get_bill_price(dom: str, price_xpath: str) -> float:
+    price = dom.xpath(price_xpath)[0].text
+    price = float(price.replace(".", "").replace(",", "."))
+    return price
+
+def get_bill_text(dom: str, bill_text_xpath: str) -> str:
+    bill = dom.xpath(bill_text_xpath)
+    bill_text = "check"
+    if len(bill) != 0:
+        bill_text = bill[0].text
+    return bill_text
+
+def get_bill_name(dom: str, bill_name_xpath: str) -> str:
+    return dom.xpath(bill_name_xpath)[0].text
