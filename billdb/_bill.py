0349b5075497b809d0f52a356f92e6726caf1f40
lumpsoid <mmgeeki@gmail.com>
2024-03-29 16:39:01 +0100

    fix check duplicates

--- billdb/_bill.py
+++ billdb/_bill.py
@@ -168,15 +168,7 @@ class Bill:
 
         self.dup_list = Bill.cursor.fetchall()
 
-    def from_qr(self, link: str, force_dup: bool = False) -> object:
-        # checking duplicates
-        self.check_duplicates()
-        if len(self.dup_list) and not force_dup:
-            Bill.LOGGER.info(
-                f"Maybe duplicates ({len(self.dup_list)}). Nothing was changed."
-            )
-            return
-
+    def from_qr(self, link: str) -> object:
         if re.match(r"https://suf.purs.gov.rs", link):
             Bill.LOGGER.debug("using serbian parser.")
             # name, date, price, currency, country, bill_text, items
@@ -191,6 +183,9 @@ class Bill:
             ) = _serbia.get_bill_info(link)
             self.exchange_rate = "1"
             self.link = link
+
+        # checking duplicates
+        self.check_duplicates()
 
         return self
 
